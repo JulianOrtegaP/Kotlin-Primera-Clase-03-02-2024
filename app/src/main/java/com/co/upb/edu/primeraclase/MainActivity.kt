f417c44d67c3d2da5725d9b479d1404dfebb18ea
B09S406est <B09S406est@MEDAULAS.UPB>
2024-02-03 08:40:41 -0500

    Initial commit

---
+++ app/src/main/java/com/co/upb/edu/primeraclase/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.co.upb.edu.primeraclase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
